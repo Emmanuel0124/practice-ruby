fec5c70918567e86698d9b97a289c43314135bb4
EmmanuelBahena <mannybahena04@gmail.com>
2023-03-22 11:17:59 -0500

    print car hash

--- store_item.rb
+++ store_item.rb
@@ -2,4 +2,4 @@ toy_car = {Car: "Skyline-Gtr", color: "Magenta", brand: "Hot-Wheels"}
 coloring_book = {Theme: "Animals", pages: 99, brand: "Crayola"}
 xylophone = {type: "Classic", brand: "Fisher-Price", keys: 8} 
 
-p toy_car[Car]
+p toy_car
