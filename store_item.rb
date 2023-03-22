ce243b2fdb15060f45db38ac23e18cf5c4f2d5e2
EmmanuelBahena <mannybahena04@gmail.com>
2023-03-22 11:58:01 -0500

    print car hash in statment

--- store_item.rb
+++ store_item.rb
@@ -2,4 +2,7 @@ toy_car = {Car: "Skyline-Gtr", color: "Magenta", brand: "Hot-Wheels"}
 coloring_book = {Theme: "Animals", pages: 99, brand: "Crayola"}
 xylophone = {type: "Classic", brand: "Fisher-Price", keys: 8} 
 
-p toy_car
+p "The #{toy_car[:Car]} is #{toy_car[color]} its made by #{toy_car[brand]}"
+
+
+class 
