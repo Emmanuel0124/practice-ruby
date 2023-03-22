380dfeb5a19b9d71a18d0e2cdb83d6c38480802e
EmmanuelBahena <mannybahena04@gmail.com>
2023-03-22 12:03:49 -0500

    add brand to car

--- store_item.rb
+++ store_item.rb
@@ -2,6 +2,6 @@ toy_car = {Car: "Skyline-Gtr", color: "Magenta", brand: "Hot-Wheels"}
 coloring_book = {Theme: "Animals", pages: 99, brand: "Crayola"}
 xylophone = {type: "Classic", brand: "Fisher-Price", keys: 8} 
 
-p "The #{toy_car[:Car]} is #{toy_car[:color]} its made by #{toy_car[brand]}"
+p "The #{toy_car[:Car]} is #{toy_car[:color]} its made by #{toy_car[:brand]}"
 
 
