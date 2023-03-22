54ca29dc8a666b12e13ea731e0f99006d44d4e78
EmmanuelBahena <mannybahena04@gmail.com>
2023-03-22 11:16:34 -0500

    print car

--- store_item.rb
+++ store_item.rb
@@ -1,3 +1,5 @@
 toy_car = {Car: "Skyline-Gtr", color: "Magenta", brand: "Hot-Wheels"}
 coloring_book = {Theme: "Animals", pages: 99, brand: "Crayola"}
-xylophone = {type: "Classic", brand: "Fisher-Price", keys: 8}
+xylophone = {type: "Classic", brand: "Fisher-Price", keys: 8} 
+
+p toy_car[Car]
