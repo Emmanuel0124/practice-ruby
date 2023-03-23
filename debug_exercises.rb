cd8f90a08d9cf738b167db423a04a8a09c728a71
EmmanuelBahena <mannybahena04@gmail.com>
2023-03-23 14:01:14 -0500

    edit orgin

--- debug_exercises.rb
+++ debug_exercises.rb
@@ -1,10 +1,11 @@
 # Exercise 3: Print a message to a time traveler.
 
-puts “Greetings! What is your year of origin?'
-origin == gets.chomp
+puts 'Greetings! What is your year of origin?'
+origin = gets.chomp.to_i
 if origin < 1900
   puts ‘That’s the past!’
 elseif origin > 1900 && origin < 2020
   puts “That’s the present!”
 elsif
   puts “That’s the future!”
+end
