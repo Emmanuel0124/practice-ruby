0ebb236528bd37e2b918e88b4a83c115797efb5e
EmmanuelBahena <mannybahena04@gmail.com>
2023-03-23 13:08:39 -0500

    start give_all_raises method

--- manager.rb
+++ manager.rb
@@ -34,6 +34,13 @@ class Manager < Employee
     # use email sending library...
     puts "Email sent!"
   end
+
+  def give_all_raises
+  
+    
+  end 
+
+
 end
 
 manager = Manager.new(first_name: "Saron", last_name: "Yitbarek", salary: 100000, active: true, employees: [employee1, employee2])
