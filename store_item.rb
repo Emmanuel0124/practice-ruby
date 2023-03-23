d49d9650e79a02d0fa7522275113cc67db0b94df
EmmanuelBahena <mannybahena04@gmail.com>
2023-03-23 09:38:32 -0500

    add print def to book class, minnor edits

--- store_item.rb
+++ store_item.rb
@@ -3,7 +3,7 @@ car1 = {
   color: "Magenta", 
   brand: "Hot-Wheels"
 }
-coloring_book = {theme: "Animals", pages: 99, brand: "Crayola"}
+coloring_book = {theme: "Animals", pages: 99, brand2: "Crayola"}
 #xylophone = {type: "classic", brand: "Fisher-Price", keys: 8} 
 
 #p "The #{toy_car[:Car]} is #{toy_car[:color]} its made by #{toy_car[:brand]}"
@@ -31,10 +31,19 @@ car1.print_info
 
 
 class Book 
-  def initialize(input_theme, input_pages)
+
+  attr_reader :input_theme, attr_reader :input_pages
+
+  def initialize(input_theme, input_pages, input_brand2)
     @theme = input_theme 
     @pages = input_pages
-     
+    @brand2 = input_brand2
       
   end
+  def print 
+p "#{theme},#{Animals}, #{brand2}"
+  end 
 end
+
+coloring_book = Book.new ("Animals", 99, "Crayola")
+coloring_book.print
